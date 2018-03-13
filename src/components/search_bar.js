d0340dba5364e585c9ec9715dda83c48785f7110
shyjues <shyju.subramanian@gmail.com>
2018-03-13 23:51:54 +1100

    again commit with state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-const SearchBar = ()=>{
-    return <input />;
-};
+
+class SearchBar extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {term: 'test'};
+
+        console.log('constructor testing');
+    }
+
+    render(){
+        return <input onChange={event=>{console.log(event.target.value)}} />;
+    }
+    
+}
 
 export default SearchBar;
