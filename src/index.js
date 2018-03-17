3cb17ac384711d451fb8d22994ad7d83bab3411e
shyjues <shyju.subramanian@gmail.com>
2018-03-18 00:29:46 +1100

    added more lessons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const youtubeApiKey = 'AIzaSyClUbKvBRUb-7a4SlG_URxbiSdLIgTMhHI';
 
@@ -14,7 +15,7 @@ class App extends Component{
 
         this.state = {videos: []};
 
-        YTSearch({key: youtubeApiKey, term:'kerala'},(videos)=>{
+        YTSearch({key: youtubeApiKey, term:'Ilaiyaraja'},(videos)=>{
             this.setState({videos});
         });
     }
@@ -22,6 +23,7 @@ class App extends Component{
     render(){
         return <div>
             <SearchBar  />
+            <VideoDetail video={this.state.videos[0]} />
             <VideoList videos={this.state.videos} />
           </div>;
 
